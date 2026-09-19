fabd9189a66efd798a73395d6f3f5ca6b38aa3d9
misterCOOL803 <ben@de-roo.org>
2026-09-19 16:36:16 +0200

    Update window_manager.hpp

--- include/bensdm/window_manager.hpp
+++ include/bensdm/window_manager.hpp
@@ -170,4 +170,11 @@ private:
     static bool wm_detected_; // true als een andere WM al SubstructureRedirect heeft
 };
 
+class WindowManager {
+public:
+    void FocusWindow(Window w);
+    void CycleFocus(int direction);
+};
+
+
 } // namespace bensdm
